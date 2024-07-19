a11c746f3ac67f778ed7119acbbd7b2d0524d148
NamesMT <dangquoctrung123@gmail.com>
2024-07-19 07:44:18 +0000

    fix(docs): components auto import dir

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({ command: _command, mode }) => {
     }),
     UnoCSS(),
     Components({
+      dirs: 'components',
       resolvers: [
         IconsResolver({
           prefix: '',
